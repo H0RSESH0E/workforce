04b3d766150df57df618c2f7af4c1dc4bad8942f
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-03-20 16:38:04 -0400

    for BCS review

--- workforce.js
+++ workforce.js
@@ -7,17 +7,34 @@ let purpose = "start";
 
 askTheUser(purpose)
     .then(userResponses => {
+
         console.log(userResponses, "<-------------9");
         purpose = userResponses.start_response;
         return askTheUser(purpose);
+
     })
     .then(userResponses => {
+
         console.log("this was to: ", purpose, "<---------------8");
-        return crud(purpose, userResponses)
+        crud(purpose, userResponses).then(table => { return table;});
+        
     })
     .then(data => {
+
         let t = data.data;
         console.table('_________>', t);
     })    
     .then(() => console.log('POOP!!!'));
+
+const run = async() => {
+    let dbEdit = {};
+
+    let task = await askTheUser(purpose);
+    purpose = task.start_response;
+}   if (purpose !== 'view all departments' || purpose !== 'view all roles' || purpose !== 'view all employees') {
+    dbEdit = await askTheUser(purpose);
+} 
+    let table = await crud(purpose, dbEdit);
+    cons
+
 
