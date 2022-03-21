7b25079bd8a9d2335c751ee3344a2851abde13ba
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-03-21 13:39:27 -0400

    Employee sql table query COMPLETE

--- utils/employeesCrud.js
+++ utils/employeesCrud.js
@@ -18,9 +18,20 @@ const updateEmployee = (userData) =>{
 const readEmployees = () =>{
     const sql =
     //  `SELECT * FROM employees`;
-    `SELECT * FROM employees empT
+    `SELECT 
+    empT.id, 
+    CONCAT_WS(' ', empT.first_name, empT.last_name) AS Name,
+    roles.title,
+    departments.dept_name,
+    roles.salary,
+    CONCAT_WS(' ', manT.first_name, manT.last_name) AS Manager 
+    FROM employees empT
     LEFT JOIN employees manT
-    ON empT.manager_id = manT.id`;
+    ON empT.manager_id = manT.id
+    INNER JOIN roles
+    ON empT.role_id = roles.id
+    INNER JOIN departments
+    ON roles.dept_id = departments.id`;
     return Promise.resolve(queryDb(sql));
 }
 
